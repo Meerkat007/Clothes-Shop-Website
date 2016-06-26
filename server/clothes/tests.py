d319764ba1a8a0d5d7bdf3f55ec37ab5f88114f4
Yuan <yuanji.yj@gmail.com>
2016-06-26 18:06:08 -0400

    added simple unit test for the clothes model

--- server/clothes/tests.py
+++ server/clothes/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Clothes
 
-# Create your tests here.
+class ClothesModelTests(TestCase):
+
+    def setUp(self):
+        Clothes.objects.create(clothes_type='ladies dress', price=28.50)
+        Clothes.objects.create(clothes_type='men tie', price=8.50)
+    
+    def test_number_of_clothes_created(self):
+        self.assertEqual(Clothes.objects.all().count(), 2)
