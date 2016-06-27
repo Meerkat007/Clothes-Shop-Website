35958cf58b65b8cb01caf52ca80d92f1a712cf23
Yuan <yuanji.yj@gmail.com>
2016-06-26 20:48:37 -0400

    added plural name

--- server/clothes/models.py
+++ server/clothes/models.py
@@ -3,3 +3,6 @@ from django.db import models
 class Clothes(models.Model):
     clothes_type = models.CharField(max_length=200)
     price = models.DecimalField(max_digits=6, decimal_places=2)
+
+    class Meta:
+        verbose_name_plural = 'clothes'
