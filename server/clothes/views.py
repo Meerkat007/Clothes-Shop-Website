af1511ed077245681b92a7ee9bda2214f61e0dda
Yuan <yuanji.yj@gmail.com>
2016-06-26 20:48:47 -0400

    created viewset

--- server/clothes/views.py
+++ server/clothes/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Clothes
+from .serializers import ClothesSerializer
 
-# Create your views here.
+class ClothesViewSet(viewsets.ReadOnlyModelViewSet):
+    """
+    This viewset automatically provides `list` and `detail` actions.
+    """
+    queryset = Clothes.objects.all()
+    serializer_class = ClothesSerializer
