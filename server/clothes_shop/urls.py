cdc2ebe53a2ca686887c8805acfd8060703ec284
Yuan <yuanji.yj@gmail.com>
2016-07-02 21:17:46 -0400

    changed url to products

--- server/clothes_shop/urls.py
+++ server/clothes_shop/urls.py
@@ -17,6 +17,6 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^clothes-shop/', include('clothes.urls')),
+    url(r'^products/', include('products.urls')),
     url(r'^admin/', admin.site.urls),
 ]
